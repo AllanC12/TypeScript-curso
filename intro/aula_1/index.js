10bc45a45de816bfe25cf7cf5b1b18d4ba01cdc8
AllanC12 <allandevfront@gmail.com>
2023-10-24 23:20:14 -0300

    primeiro programa em TS

---
+++ intro/aula_1/index.js
@@ -0,0 +1,5 @@
+var myExpression = 4 * 9 + (120 / 2);
+var resolve = function (expression) {
+    console.log(expression);
+};
+resolve(myExpression);
---
+++ intro/aula_1/index.ts
@@ -0,0 +1,10 @@
+const myExpression = 4 * 9 + (120 / 2)
+
+const resolve = (expression: number) => {
+    console.log(expression) 
+}
+
+resolve(myExpression)
+
+
+
