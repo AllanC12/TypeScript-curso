ca77a717ef11d1f28c29eea4b564fa45311bf9a7
AllanC12 <allandevfront@gmail.com>
2023-11-18 21:34:50 -0300

    narrowing por checagem de existência de valor

--- narrowing/index.js
+++ narrowing/index.js
@@ -12,6 +12,24 @@ function sumNumber(a, b) {
         console.log('Impossível realizar a soma');
     }
 }
-sumNumber(2, 2);
-sumNumber('12', '123');
-sumNumber('23123', 12312);
+// sumNumber(2,2)
+// sumNumber('12','123')
+// sumNumber('23123',12312)
+// checando se valor existe
+function operation(arr, operation) {
+    if (operation) {
+        if (operation === "sum") {
+            const sum = arr.reduce((acc, index) => acc + index);
+            console.log(sum);
+        }
+        else if (operation === "multiply") {
+            const multiply = arr.reduce((acc, index) => acc * index);
+            console.log(multiply);
+        }
+    }
+    else {
+        console.log('Insira uma operação');
+    }
+}
+operation([1, 2, 3], 'sum');
+operation([2, 4, 7], 'multiply');
--- narrowing/index.ts
+++ narrowing/index.ts
@@ -12,6 +12,25 @@ function sumNumber(a: number | string, b: number | string) {
     }
 }
 
-sumNumber(2,2)
-sumNumber('12','123')
-sumNumber('23123',12312)
+// sumNumber(2,2)
+// sumNumber('12','123')
+// sumNumber('23123',12312)
+
+// checando se valor existe
+
+function operation (arr: number[],operation?: string | undefined){
+    if(operation){
+        if(operation === "sum"){
+           const sum = arr.reduce((acc,index) => acc + index)
+           console.log(sum)
+        }else if(operation === "multiply"){
+           const multiply = arr.reduce((acc,index) => acc * index)
+           console.log(multiply)
+        }
+    }else{
+        console.log('Insira uma operação')
+    }
+}
+
+operation([1,2,3],'sum')
+operation([2,4,7],'multiply')
