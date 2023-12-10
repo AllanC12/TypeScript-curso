3f74c37c40e47c1c62495522d5a8b1e5705f3fab
AllanC12 <allandevfront@gmail.com>
2023-12-09 23:26:17 -0300

    revisando generics

---
+++ create_types/index.ts
@@ -0,0 +1,18 @@
+// Há a possibilidade de gerar novos tipos em Typescript , ja vimos isso com generics.
+// Porém existem outros operadores que visam facilitar nossa vida neste assunto
+// A ideia deste recurso é deixar a manutenção do projeto mais simples
+// Ou seja, gastar mais tempo na estruturação dos tipos e menos na busca de possíveis bugs depois
+
+// Revisão de generics
+// Utilizamos generics quando uma função pode aceitar mais de um tipo
+// É uma prática mais interessante que o any, que teria um efeito parecido
+
+
+//como meu argumento recebe o generic e a função tem um retorno de string, independente do argumento que eu passe para a função,
+// o generic fará essa conversão para o tipo retornado pela função
+
+function showMessage<T>(arg: T): string{
+  return `O argumento é: ${arg}`
+}
+
+console.log(showMessage(3))
