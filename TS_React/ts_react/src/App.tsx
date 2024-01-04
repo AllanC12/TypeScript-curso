6279df4daace3313aa206f9cea02f11e19ed050f
AllanC12 <allandevfront@gmail.com>
2024-01-03 21:13:30 -0300

    iniciando com react e ts

--- TS_React/ts_react/src/App.tsx
+++ TS_React/ts_react/src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import FirstComponent from './components/FirstComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const name: string = "Allan"
+  const age: number = 23
+  const isWorking: boolean = true
+
+  const userGreeting = (name: string): string => {
+    return `Olá ${name}`
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Typescript com React</h1>
+     <h3>Nome: {name}</h3>
+     <h3>Idade: {age}</h3>
+     {isWorking ? (
+      <h3>Está trabalhando: Sim</h3>
+     ) : (
+       <h3>Está trabalhando: Não</h3>
+     )}
+     <h3>{userGreeting(name)}</h3>
+     <FirstComponent/>
     </>
   )
 }
