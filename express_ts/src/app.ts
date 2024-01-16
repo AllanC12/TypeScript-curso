333eefa9cce045befda0202c8e5b945ee08be6a5
AllanC12 <allandevfront@gmail.com>
2024-01-15 22:31:23 -0300

    mais conceitos de express

--- express_ts/src/app.ts
+++ express_ts/src/app.ts
@@ -1,7 +1,7 @@
 
 // init express
 
-import express from 'express'
+import express,{Request,Response} from 'express'
 
 const app = express()
 
@@ -28,6 +28,59 @@ app.all('/api/product/check',(req,res) => {
      return res.send('Não podemos realizar esta operação :(')
    }
 })
+
+// 5 ---> Interfaces do express
+app.get('/api/interfaces',(req:Request,res:Response) => {
+  return res.send("Utilizando as interfaces")
+})
+
+// 6 ---> Enviando JSON
+app.post('/api/json',(req:Request,res:Response) => {
+    return res.send({
+        name: "shirt",
+        price: 30.00,
+        color: "Black",
+        sizes: ["P","M","G"]
+    })
+})
+
+// 7 ---> Router parameters
+
+app.get('/api/product/:id',(req:Request,res:Response) => {
+    console.log(req.params)
+
+    const id = req.params.id
+
+    if(id === '1'){
+        const product = {
+            id: 1,
+            name: 'Jaqueta',
+            price: 134.99
+        }
+      return res.json(product)
+    }else{
+      return res.send('Produto não encontrado')
+    }
+})
+
+// 8 ---> Rotas mais complexas
+
+app.get('/api/product/:productId/review/:reviewId',(req:Request,res:Response) => {
+    const productId = req.params.productId
+    const reviewId = req.params.reviewId
+
+    return res.send(`Estamos acessando a review ${reviewId} do produto ${productId}`)
+})
+
+// 9 ---> Router handler
+
+ const getUser = (req: Request, res: Response) => {
+   console.log('Resgatando usuário com id: ' + req.params.id)
+   return res.send("Usuário encontrado no sistema")
+ }
+
+ app.get('/api/user/:id',getUser)
+
 
 
 app.listen(3000, () => {
